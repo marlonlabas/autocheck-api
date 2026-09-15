6e39ec2233a9104aadc642d262cb24b579e2e888
Marlon Labas <marlon_lab@hotmail.com>
2026-09-14 21:52:16 -0300

    Add database context configuration and update appsettings for connection string

--- src/AutoCheck.Api/Program.cs
+++ src/AutoCheck.Api/Program.cs
@@ -1,4 +1,9 @@
+using AutoCheck.Api.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AutoCheckDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Add services to the container.
 
